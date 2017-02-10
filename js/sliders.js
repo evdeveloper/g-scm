2a61ebe2f7941c3604d70089fdc22ba114b998d3
andreyalexeich <andrey.alexeich@gmail.com>
2017-02-10 17:15:32 +0300

    Revert "Удалены html-файлы"
    This reverts commit 3b0b9be7fa2498641d32b89180da2ac77c032805.

--- js/sliders.js
+++ js/sliders.js
@@ -1,85 +1,89 @@
 $(document).ready(function() {
-    var main__slider = $("#main__slider").slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false,
-        arrows: false
+    var main_slider = $('#main__slider').lightSlider({
+        item: 1,
+        pager: false,
+        slideMargin: 0,
+        controls: false,
+        auto: true,
+        loop: true,
+        enableDrag: false,
+        pauseOnHover: true,
+        pause: 6000
     });
 
-    $("#main__slider-right").click(function() {
-        main__slider.slick("slickNext");
-    }); 
-
-    $("#main__slider-left").click(function() {
-        main__slider.slick("slickPrev");
+    $('#main__slider-left').click(function(){
+        main_slider.goToPrevSlide(); 
     });
 
-
-
-    var products__slider = $(".products__slider").slick({
-        infinite: true,
-        autoplay: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        dots: false,
-        variableWidth: true,
-        arrows: false
-    });
-
-    $("#products__slider-right").click(function() {
-        products__slider.slick("slickNext");
-    }); 
-
-    $("#products__slider-left").click(function() {
-        products__slider.slick("slickPrev");
-    });
-
-    $("#sale__slider-right").click(function() {
-        products__slider.slick("slickNext");
-    }); 
-
-    $("#sale__slider-left").click(function() {
-        products__slider.slick("slickPrev");
+    $('#main__slider-right').click(function(){
+        main_slider.goToNextSlide(); 
     });
 
 
 
-    var certificates__slider = $("#certificates__slider").slick({
-        infinite: true,
-        autoplay: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        dots: false,
-        variableWidth: true,
-        arrows: false
+    var products_slider = $('#products__slider').lightSlider({
+        item: 4,
+        pager: false,
+        adaptiveHeight: true,
+        slideMargin: 75,
+        controls: false,
+        auto: true,
+        loop: true,
+        enableDrag: false,
+        pauseOnHover: true,
+        pause: 6000
     });
 
-    $("#certificates__slider-right").click(function() {
-        certificates__slider.slick("slickNext");
-    }); 
+    $('#products__slider-left').click(function(){
+        products_slider.goToPrevSlide(); 
+    });
 
-    $("#certificates__slider-left").click(function() {
-        certificates__slider.slick("slickPrev");
+    $('#products__slider-right').click(function(){
+        products_slider.goToNextSlide(); 
     });
 
 
 
-    var manufacturers__slider = $(".manufacturers__slider").slick({
-        infinite: true,
-        autoplay: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        dots: false,
-        variableWidth: true,
-        arrows: false
+
+    var certificates_slider = $('#certificates__slider').lightSlider({
+        item: 4,
+        pager: false,
+        slideMargin: 56,
+        controls: false,
+        auto: true,
+        loop: true,
+        enableDrag: false,
+        pauseOnHover: true,
+        pause: 6000
     });
 
-    $("#manufacturers__slider-right").click(function() {
-        manufacturers__slider.slick("slickNext");
-    }); 
+    $('#certificates__slider-left').click(function(){
+       certificates_slider.goToPrevSlide(); 
+    });
 
-    $("#manufacturers__slider-left").click(function() {
-        manufacturers__slider.slick("slickPrev");
+    $('#certificates__slider-right').click(function(){
+       certificates_slider.goToNextSlide(); 
+    });
+
+
+
+    var manufacturers_slider = $('#manufacturers__slider').lightSlider({
+        item: 5,
+        pager: false,
+        slideMargin: 56,
+        controls: false,
+        auto: true,
+        loop: true,
+        enableDrag: false,
+        pauseOnHover: true,
+        pause: 6000
+    });
+
+    $('#manufacturers__slider-left').click(function(){
+       manufacturers_slider.goToPrevSlide(); 
+    });
+
+    $('#manufacturers__slider-right').click(function(){
+       manufacturers_slider.goToNextSlide(); 
     });
 });
