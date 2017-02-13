e150aa12f055a59348b1ddbe6aa705e50d98b70e
andreyalexeich <andrey.alexeich@gmail.com>
2017-02-13 19:43:05 +0300

    Изменены блоки сайта

---
+++ includes/blocks/header.php
@@ -0,0 +1,19 @@
+<header class="header default">
+	<div class="fixed">
+		<a href="#" class="header__logo"><img src="images/logo.svg" alt=""></a>
+		<div class="header__right-section">
+			<div class="right-section__address">
+				<p>Москва, 5-я Магистральная, д. 12 Время работы: ПН-ПТ 09:00 - 18:00
+				<a href="mailto:info@g-scm.ru">info@g-scm.ru</a></p>
+			</div>
+			<div class="right-section__callback">
+				<h2 class="right-section__number">8 (495) 663 9033</h2>
+				<a href="#" class="right-section__callback">Обратный звонок</a>
+			</div>
+			<div class="right-section__menu">
+				<a href="#" class="right-section__cabinet">Личный кабинет</a>
+				<a href="#" class="right-section__cart">Корзина <span>пуста</span></a>
+			</div>
+		</div>
+	</div>
+</header>
---
+++ includes/sliders/similar_products.php
@@ -0,0 +1,102 @@
+<div class="slider-controls">
+	<span class="slider-controls__left" id="products-slider__left"></span>
+	<span class="slider-controls__right" id="products-slider__right"></span>
+</div>
+<div class="slider-products">
+	<div class="product-item product-item_margin">
+		<div class="product-item__img">
+			<img src="images/products/similar-products/item_1.jpg" alt="">
+		</div>
+		<div class="product-item__description">
+			<p>КЛАПАН ОБРАТНЫЙ ДВУХСТВОРЧАТЫЙ ZETKAMA 407A</p>
+		</div>
+		<span class="product-item__price">ОТ 2 377 &#8381;</span>
+		<div class="product-item__btn">
+			<a href="#">В корзину</a>
+		</div>
+	</div>
+	<div class="product-item product-item_margin">
+		<div class="product-item__img">
+			<img src="images/products/similar-products/item_2.jpg" alt="">
+		</div>
+		<div class="product-item__description">
+			<p>КЛАПАН ОБРАТНЫЙ ДВУХСТВОРЧАТЫЙ DANFOSS ТИП…</p>
+			<span class="product-item__price">ОТ 10 260 &#8381;</span>
+		</div>
+		<div class="product-item__btn">
+			<a href="#">В корзину</a>
+		</div>
+	</div>
+	<div class="product-item product-item_margin">
+		<div class="product-item__img">
+			<img src="images/products/similar-products/item_3.jpg" alt="">
+		</div>
+		<div class="product-item__description">
+			<p>КЛАПАН ОБРАТНЫЙ ДВУХСТВОРЧАТЫЙ DANFOSS ТИП…</p>
+			<span class="product-item__price">ОТ 8 853 &#8381;</span>
+		</div>
+		<div class="product-item__btn">
+			<a href="#">В корзину</a>
+		</div>
+	</div>
+	<div class="product-item product-item_margin">
+		<div class="product-item__img">
+			<img src="images/products/similar-products/item_4.jpg" alt="">
+		</div>
+		<div class="product-item__description">
+			<p>КЛАПАН ОБРАТНЫЙ ПОДЪЕМНЫЙ ZETKAMA 277A</p>
+			<span class="product-item__price">ОТ 1 341 &#8381;</span>
+		</div>
+		<div class="product-item__btn">
+			<a href="#">В корзину</a>
+		</div>
+	</div>
+	<div class="product-item product-item_margin">
+		<div class="product-item__img">
+			<img src="images/products/similar-products/item_1.jpg" alt="">
+		</div>
+		<div class="product-item__description">
+			<p>КЛАПАН ОБРАТНЫЙ ДВУХСТВОРЧАТЫЙ ZETKAMA 407A</p>
+		</div>
+		<span class="product-item__price">ОТ 2 377 &#8381;</span>
+		<div class="product-item__btn">
+			<a href="#">В корзину</a>
+		</div>
+	</div>
+	<div class="product-item product-item_margin">
+		<div class="product-item__img">
+			<img src="images/products/similar-products/item_2.jpg" alt="">
+		</div>
+		<div class="product-item__description">
+			<p>КЛАПАН ОБРАТНЫЙ ДВУХСТВОРЧАТЫЙ DANFOSS ТИП…</p>
+			<span class="product-item__price">ОТ 10 260 &#8381;</span>
+		</div>
+		<div class="product-item__btn">
+			<a href="#">В корзину</a>
+		</div>
+	</div>
+	<div class="product-item product-item_margin">
+		<div class="product-item__img">
+			<img src="images/products/similar-products/item_3.jpg" alt="">
+		</div>
+		<div class="product-item__description">
+			<p>КЛАПАН ОБРАТНЫЙ ДВУХСТВОРЧАТЫЙ DANFOSS ТИП…</p>
+			<span class="product-item__price">ОТ 8 853 &#8381;</span>
+		</div>
+		<div class="product-item__btn">
+			<a href="#">В корзину</a>
+		</div>
+	</div>
+	<div class="product-item product-item_margin">
+		<div class="product-item__img">
+			<img src="images/products/similar-products/item_4.jpg" alt="">
+		</div>
+		<div class="product-item__description">
+			<p>КЛАПАН ОБРАТНЫЙ ПОДЪЕМНЫЙ ZETKAMA 277A</p>
+			<span class="product-item__price">ОТ 1 341 &#8381;</span>
+		</div>
+		<div class="product-item__btn">
+			<a href="#">В корзину</a>
+		</div>
+	</div>
+</div>
